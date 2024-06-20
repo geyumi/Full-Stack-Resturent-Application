153b792535706ee9bbb3183602032567d09c2eba
geyumi <geyumisrisara@gmail.com>
2024-06-20 13:40:10 +0530

    new Api configuration

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { Link } from "react-router-dom";
 import { HiOutlineArrowRight } from "react-icons/hi";
 
@@ -10,7 +10,7 @@ const About = () => {
           <div className="banner">
             <div className="top">
               <h1 className="heading">ABOUT US</h1>
-              <p>The only thing we're serious about is food.</p>
+              <p>The only thing we are serious about is food.</p>
             </div>
             <p className="mid">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
